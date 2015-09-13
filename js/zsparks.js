d7171c7b2a82f39bf42beb416c21520bc95da155
unknown <jlindsay1@ggc.edu>
2015-09-12 23:37:56 -0400

    added the page to page functionality and BEGAN adding a theme across all pages

---
+++ js/zsparks.js
@@ -0,0 +1,48 @@
+/*home page scripts*/
+$(document).ready(function()
+{
+	function login()
+	{
+		window.open ('profile.html','_self')
+	}
+	$("#loginbtn").click(login);
+	
+	function register()
+	{
+		window.open ('signup.html','_self',false)
+	}
+	$("#registerbtn").click(register);
+});
+
+
+/*registration page scripts*/
+$(document).ready(function()
+{
+	function completeregistration()
+	{
+		window.open ('administration.html','_self')	
+	}
+	$("#completeregistrationbtn").click(completeregistration);
+});
+
+
+/*profile page scripts*/
+$(document).ready(function()
+{
+	function editbtn()
+	{
+		//todo
+	}
+	$("#id_here").click(editbtn);
+});
+
+
+/*admin page script*/
+$(document).ready(function()
+{
+	function exportbtn()
+	{
+		//todo
+	}
+	$("#id_here").click(exportbtn);
+});
