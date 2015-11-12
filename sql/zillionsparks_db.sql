9738da6d7d27251151b69bfdf2d89d0c4107afb0
JJLindsay <jlindsay1@ggc.edu>
2015-11-12 16:44:29 -0500

    put the database back.

--- sql/zillionsparks_db.sql
+++ sql/zillionsparks_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2015 at 03:05 AM
+-- Generation Time: Nov 08, 2015 at 03:02 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -23,10 +23,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gamescore`
+-- Table structure for table `dilemmas`
 --
 
-CREATE TABLE IF NOT EXISTS `gamescore` (
+CREATE TABLE IF NOT EXISTS `dilemmas` (
   `id` int(3) NOT NULL,
   `p1` text NOT NULL,
   `p2` text NOT NULL,
@@ -37,13 +37,13 @@ CREATE TABLE IF NOT EXISTS `gamescore` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `gamescore`
+-- Dumping data for table `dilemmas`
 --
 
-INSERT INTO `gamescore` (`id`, `p1`, `p2`, `p1_choice`, `p2_choice`, `p1_score`, `p2_score`) VALUES
-(1, 'A1', 'B1', NULL, NULL, 0, 0),
-(2, 'B1', 'C1', NULL, NULL, 0, 0),
-(3, 'C1', 'A1', NULL, NULL, 0, 0);
+INSERT INTO `dilemmas` (`id`, `p1`, `p2`, `p1_choice`, `p2_choice`, `p1_score`, `p2_score`) VALUES
+(1, 'A1', 'B1', 0, 1, 6, 0),
+(2, 'B1', 'C1', 1, 1, 3, 3),
+(3, 'C1', 'A1', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -68,21 +68,21 @@ INSERT INTO `teamcode` (`users_id`, `tag`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `totalscore`
+-- Table structure for table `totals`
 --
 
-CREATE TABLE IF NOT EXISTS `totalscore` (
+CREATE TABLE IF NOT EXISTS `totals` (
   `users_id` int(4) NOT NULL,
   `totalscore` int(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `totalscore`
+-- Dumping data for table `totals`
 --
 
-INSERT INTO `totalscore` (`users_id`, `totalscore`) VALUES
+INSERT INTO `totals` (`users_id`, `totalscore`) VALUES
 (1, 0),
-(2, 0),
+(2, 40),
 (3, 0);
 
 -- --------------------------------------------------------
@@ -101,26 +101,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pw` varchar(255) NOT NULL,
   `confirmed_pw` varchar(255) NOT NULL,
   `course` varchar(12) DEFAULT NULL,
-  `section` int(2) DEFAULT NULL
+  `section` int(2) DEFAULT NULL,
+  `online_status` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `usernames`, `first_name`, `last_name`, `email`, `confirmed_email`, `pw`, `confirmed_pw`, `course`, `section`) VALUES
-(1, 'mmodi26', 'Mitesh', 'Modi', 'mitesh.modi003@gmail.com', 'mitesh.modi003@gmail.com', 'nirmauni', 'nirmauni', NULL, NULL),
-(2, 'jpatel90', 'Jay', 'Patel', 'jpatel@gmail.com', 'jpatel@gmail.com', 'nirmauni', 'nirmauni', NULL, NULL),
-(3, 'hpatel91', 'Harshal', 'Patel', 'hpatel91@gmail.com', 'hpatel91@gmail.com', 'nirmauni', 'nirmauni', NULL, NULL);
+INSERT INTO `users` (`id`, `usernames`, `first_name`, `last_name`, `email`, `confirmed_email`, `pw`, `confirmed_pw`, `course`, `section`, `online_status`) VALUES
+(1, 'mmodi26', 'Mitesh', 'Modi', 'mitesh.modi003@gmail.com', 'mitesh.modi003@gmail.com', 'nirmauni', 'nirmauni', NULL, NULL, 0),
+(2, 'jpatel90', 'Jay', 'Patel', 'jpatel@gmail.com', 'jpatel@gmail.com', 'nirmauni', 'nirmauni', 'Biology', 4, 1),
+(3, 'hpatel91', 'Harshal', 'Patel', 'hpatel91@gmail.com', 'hpatel91@gmail.com', 'nirmauni', 'nirmauni', NULL, NULL, 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `gamescore`
+-- Indexes for table `dilemmas`
 --
-ALTER TABLE `gamescore`
+ALTER TABLE `dilemmas`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -130,9 +131,9 @@ ALTER TABLE `teamcode`
   ADD PRIMARY KEY (`users_id`);
 
 --
--- Indexes for table `totalscore`
+-- Indexes for table `totals`
 --
-ALTER TABLE `totalscore`
+ALTER TABLE `totals`
   ADD PRIMARY KEY (`users_id`);
 
 --
@@ -146,9 +147,9 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `gamescore`
+-- AUTO_INCREMENT for table `dilemmas`
 --
-ALTER TABLE `gamescore`
+ALTER TABLE `dilemmas`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
